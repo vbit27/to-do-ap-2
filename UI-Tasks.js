51c6fa9be24baa477f05acb85d20f1213a517740
Vasil Bit <vasil.bituni@gmail.com>
2021-03-26 10:55:05 +0100

    Add fix bug with updateStatus

--- UI-Tasks.js
+++ UI-Tasks.js
@@ -6,8 +6,6 @@ import { activeProject, allProjects, createNewTask } from './Manager.js'
 export function updateActiveProject(index) {
     updateProjectTitle(index);
     renderTasks(index);
-
-
 }
 
 
@@ -17,10 +15,9 @@ export function updateActiveProject(index) {
 
 const updateProjectTitle = (index) => {
     const activeProjectDetails = document.querySelector('.active-project-description')
-
+    activeProjectDetails.innerHTML = ''
 
     if (allProjects.length) {
-        activeProjectDetails.innerHTML = ''
 
         const activeTitle = document.createElement('h3');
         activeTitle.innerText = allProjects[index].name;
@@ -30,16 +27,12 @@ const updateProjectTitle = (index) => {
 
         activeProjectDetails.appendChild(activeTitle)
         activeProjectDetails.appendChild(activeDescription)
-    } else {
-        activeProjectDetails.innerHTML = ''
     }
 }
 
 const addTaskButton = document.querySelector('.add-task');
 
 //addTaskButton.addEventListener('click', addNewTask)
-
-
 
 
 
@@ -59,8 +52,6 @@ function submitTask(ev) {
     
     createNewTask(name, description, dueDate, priority)
     renderTasks(activeProject)
-    setListenersToTaks();
-
 }
 
 
@@ -74,29 +65,34 @@ function renderTasks(index) {
     const taskContainer = document.querySelector('.task-list-container')
     taskContainer.innerHTML = '';
 
-        if(allProjects.length && allProjects[index].tasks.length) {
-            for (let task of allProjects[index].tasks) {
-                const singleTask = document.createElement('div')
-                const circle = document.createElement('span')
-                const taskTitle = document.createElement('div')
-                const editTaskBtn = document.createElement('button')
-                const deleteTaskBtn = document.createElement('button')
+    if(allProjects.length && allProjects[index].tasks.length) {
+        for (let task of allProjects[index].tasks) {
+            const singleTask = document.createElement('div')
+            const titleContainer = document.createElement('div')
+            const circle = document.createElement('span')
+            const taskTitle = document.createElement('div')
+            const editTaskBtn = document.createElement('button')
+            const deleteTaskBtn = document.createElement('button')
 
-                singleTask.classList.add('task')
-                circle.classList.add('dot')
-                taskTitle.innerText = task.name
-                editTaskBtn.innerText = 'Edit';
-                editTaskBtn.classList.add('edit-task')
-                deleteTaskBtn.innerText = 'X';
-                deleteTaskBtn.classList.add('delete-task')
+            singleTask.classList.add('task')
+            titleContainer.classList.add('task-title-container')
+            circle.classList.add('dot')
+            taskTitle.innerText = task.name
+            editTaskBtn.innerText = 'Edit';
+            editTaskBtn.classList.add('edit-task')
+            deleteTaskBtn.innerText = 'X';
+            deleteTaskBtn.classList.add('delete-task')
 
-                singleTask.appendChild(circle)
-                singleTask.appendChild(taskTitle)
-                singleTask.appendChild(editTaskBtn)
-                singleTask.appendChild(deleteTaskBtn)
-                taskContainer.appendChild(singleTask)
-            }
+            titleContainer.appendChild(circle)
+            titleContainer.appendChild(taskTitle)
+            singleTask.appendChild(titleContainer)
+            singleTask.appendChild(editTaskBtn)
+            singleTask.appendChild(deleteTaskBtn)
+            taskContainer.appendChild(singleTask)
         }
+
+        setListenersToTaks();
+    }
 
 }
 
@@ -104,19 +100,19 @@ function renderTasks(index) {
 // ADD TASK FUNCTIONALITIES 
 
 function setListenersToTaks() {
-    const taskElements = document.querySelectorAll('.task-list-container .task')
+    
+    const taskElements = document.querySelectorAll('.task-title-container')
     const deleteTaskBtn = document.querySelectorAll('.delete-task')
     const editTaskBtn = document.querySelectorAll('.edit-task')
 
 
     function removeTask() {
         this.parentNode.remove()
-    
+
         let index = Array.from(deleteTaskBtn).indexOf(this)
-        allProjects[activeProject].removeTask(index)
-    
+        allProjects[activeProject].deleteTask(index)
+
         renderTasks(activeProject)
-        setListenersToTaks()
     }
     
     function updateTaskStatus() {
